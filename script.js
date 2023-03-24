8b96866ac943edbb31c393cae7db7b4929bc1a57
Nuhazet <nuhazet1997@gmail.com>
2023-03-24 17:02:14 +0000

    kkck

--- script.js
+++ script.js
@@ -1,30 +1,76 @@
-let alien = {
-    x: 9,
-    y: 16,
-    direction: 'left'
 
-};
+let allien = {
+    pos: {
+        x: 9,
+        y: 16
+    },
+    direction: "left",
+}
+
+
 
 //document.getElementById()
+function drawAllien() {
+    var allienCell = document.querySelector(`.row${allien.pos.y} .col${allien.pos.x}`)
+    console.log(allienCell)
+    allienCell.classList.add('allien')
+}
+
+
+/*
+
+
+    switch(allien.direction){
+        case'left': allien.pos.x = 8
+        break;
+        case 'right': allien.pos.x ++
+        break;
+        case 'up': allien.pos.y--
+        break;
+        case 'down': allien.pos.y++
+        break;
+
+*/
+
+
+function moveAllien () {
+
+    switch(allien.direction){
+        case "left":
+            allien.pos.x--;
+
+        break;
 
 
 
 
+    }
 
+/*
+    if(allien.direction == "right" && allien.pos.x < 17){
+        allien.pos.x = 10
+    }
+    */
+ 
+}
+
+    
 //DETECTA MOVIVIMIENTO CON FLECHAS
 window.addEventListener('keydown', function (event) {
     switch (event.code) {
         case "ArrowLeft":
-            alien.direction = "left"
-            break
+            allien.direction = "left"
+            break;
         case "ArrowRight":
-            alien.direction = "right"
-            break
+            allien.direction = "right"
+            break;
         case "ArrowUp":
-            alien.direction = "up"
-            break
+            allien.direction = "up"
+            break;
         case "ArrowDown":
-            alien.direction = "down"
-            break
+            allien.direction = "down"
+            break;
     }
 })
+drawAllien()
+moveAllien()
